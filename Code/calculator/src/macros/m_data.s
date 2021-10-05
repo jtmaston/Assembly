247bc7b2caab6306669e26529d53446446fc0451
jtmaston <jtmstn1@gmail.com>
2021-10-05 19:58:36 +0300

    Stage 1 complete [ brief ] * Implemented addition and substrction * Implemented printing of the result * Commented most of the code * Changed some macros around

--- Code/calculator/src/macros/m_data.s
+++ Code/calculator/src/macros/m_data.s
@@ -1,9 +1,11 @@
-.macro getptr var
+; describes macros that manipulate data
+                                
+.macro getptr var               ; gets a pointer to a label, returnx in X0
     adrp X0, \var@PAGE         
-    add X0, X0, \var@PAGEOFF     ; load prompt_1 pointer into X0.
+    add X0, X0, \var@PAGEOFF
 .endm
 
-.macro saveptr var
+.macro saveptr var              ; get pointer to label, pushing it onto the stack
     getptr \var
     push X0
 .endm
--- Code/calculator/src/main.s
+++ Code/calculator/src/main.s
@@ -1,30 +1,38 @@
-# This example aims to make a small program to read user input and perform a simple addition.
-# Very WIP.
-# TODO:
-# * Adding logic
-# Done:
-# * Printing ( I'm stubborn and actively refuse to use printf )
-# * Input
+; This example aims to make a small program to read user input and perform a simple addition.
+; Very WIP.
+; TODO:
+;   * Multiplcation and division
+;   * Parantheses 
+;   * General code cleanup
+; Done:
+;   * Printing ( I'm stubborn and actively refuse to use printf )
+;   * Input
+;   * Addition & substraction
+
 
 .include "macros/macros.s"
-.global _main, input_string, char_input
+.global _main, input_string, output
 
 .align 4
 .text
 .arch armv8-a
 
 _main:
-    saveptr prompt                  ; get the pointer of the prompt, save in slot 1
-    saveptr cursor                  ; get the pointer of the cursor, save in slot 2
-    saveptr input_string            ; get the pointer of the cursor, save in slot 3
-    str XZR, [X0]                   ; also initialize input_string with 0
-
-    print #1                        ; print prompt
+    saveptr prompt                  ; << 
+    saveptr cursor                  ; save the pointers to the many prompts
+    saveptr output                  ;
+    saveptr input_string            ; >>
+    print #1                        ; print the welcome prompt
+    
+loop:
     print #2                        ; print cursor
-
+    peek X0                         ; get the address of input_string into X9
+    str XZR, [X0]                   ; also initialize input_string with 0
     bl input                        ; branch to the input function
+    bl parse                        ; once returned from input, parse it
+    print #3                        ; print the result
 
-    #bl parse                       ; once returned from input, parse it
+    b loop                          ; and go back to the beginning
 
     mov sp, fp                      ; restore the sp and fp
 
@@ -35,13 +43,10 @@ _main:
 .bss
 .align 16
     input_string: .skip 101         ; reserve max 100 bytes
+    output: .skip 30
 
 .data
 .align 16
-    prompt: .asciz "Welcome! \nThis is a simple calculator. Input your equation and it will solve it. \nEnjoy!\n"
-    cursor: .asciz "> "
-    
-
-/*
- */
-    
+    prompt: .asciz "Welcome! \nThis is a simple calculator. Input your equation and it will solve it. \nEnjoy!"
+    cursor: .asciz "\n> "
+    newline: .asciz ""
